032f463a2a88734219b63108454e432b42b03666
Michal Orlik <sogehige@gmail.com>
2021-10-19 13:22:06 +0200

    feat(eventsub): add eventsub redirect

---
+++ pages/_oauth/eventsub.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-overlay dark>
+      <v-row>
+        <v-col class="text-center">
+          <v-progress-circular indeterminate size="48" />
+        </v-col>
+      </v-row>
+    </v-overlay>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup () {
+    onMounted(() => {
+      location.href = `${location.origin}#/settings/modules/core/eventsub?success=true`
+    })
+
+    return {}
+  }
+})
+</script>
