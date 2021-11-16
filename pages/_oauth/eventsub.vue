b327232df286fc53be064b2d1553438a4b8d9545
Michal Orlik <sogehige@gmail.com>
2021-11-16 12:50:54 +0100

    feat(eventsub): reset state on eventsub auth

--- pages/_oauth/eventsub.vue
+++ pages/_oauth/eventsub.vue
@@ -18,9 +18,12 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import { mdiCheckboxMarkedCircleOutline } from '@mdi/js'
+import { getSocket } from '@sogebot/ui-helpers/socket'
 
 export default defineComponent({
   setup () {
+    const socket = getSocket('/services/twitch')
+    socket.emit('eventsub::reset')
     return { mdiCheckboxMarkedCircleOutline }
   }
 })
